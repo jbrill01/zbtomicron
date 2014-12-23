030276a71715f569ade8db83532b025312192b99
Jason Brillon <Jason62513@gmail.com>
2014-12-23 14:54:21 -0500

    fixed h5 fontsize

--- resizing.js
+++ resizing.js
@@ -1,4 +1,6 @@
 //Scale cover photo and everything else//
+var h5font;
+
 $(document).ready(function() {
   $('#t1').css('text-decoration','underline');
    var windowHeight = $(window).height();
@@ -9,7 +11,7 @@ $(document).ready(function() {
    var currheight = 300*percentage;
    var h1font = 30*percentage+3;
    var h4font = 18*percentage+3;
-   var h5font = 17*percentage;
+   h5font = 17*percentage+3;
    var pfont = 10*percentage+3;
    $('.box').css('height',currheight+'px');
    $('.box').css('width',currheight+'px');
@@ -334,6 +336,7 @@ function togText1() {
   $('#t5').css('text-decoration', 'none');
   $('#t6').css('text-decoration', 'none');
   $('#toggleText').html("<h5>President: Matthew Felsenfeld</h5><h5>Vice President: Joseph Cirone</h5><h5>Brotherhood Development Director: Ryan Hastings-Echo</h5><h5>Secretary: Jacob Mittleman</h5><h5>Treasurer: Jonathan Sirota</br></br></br></br></br></br></br></h5>");
+  $('#toggleText h5').css('font-size', h5font+'px');
 }
 
 
@@ -345,6 +348,7 @@ function togText2() {
   $('#t6').css('text-decoration', 'none');
   $('#t2').css('text-decoration', 'underline');
   $('#toggleText').html("<h5>Andrew Carp: E'15</h5><h5>Zachary Fialkow: A'15</h5><h5>Daniel Griffin: E'15</h5><h5>Jonathan Wolf: A'15</br></br></br></br></br></br></br></br></br></h5>");
+  $('#toggleText h5').css('font-size', h5font+'px');
 }
 
 function togText3() {
@@ -355,6 +359,7 @@ function togText3() {
   $('#t6').css('text-decoration', 'none');
   $('#t3').css('text-decoration', 'underline');
   $('#toggleText').html("<h5>Jason Brillon: A'16</h5><h5>Steven Carbone: A'15</h5><h5>Daniel Diaz: A'15</h5><h5>Rayn Riel: A'15</h5><h5>Mitul Rathod: A'16</h5><h5>Justin Silva: A'15</h5><h5>Ray Xiao: A'15</br></br></br></br></h5>");
+  $('#toggleText h5').css('font-size', h5font+'px');
 }
 
 function togText4() {
@@ -365,6 +370,7 @@ function togText4() {
   $('#t6').css('text-decoration', 'none');
   $('#t4').css('text-decoration', 'underline');
   $('#toggleText').html("<h5>John Taylor Armstrong: A'15</h5><h5>Bryan Dumond: A'15</h5><h5>Christopher Gambro: A'15</h5><h5>David Hernandez: E'15</h5><h5>Bob Liu: A'15</h5><h5>Nikhil Shinday: E'16</br></br></br></br></br></br></h5>");
+  $('#toggleText h5').css('font-size', h5font+'px');
 }
 
 function togText5() {
@@ -375,6 +381,7 @@ function togText5() {
   $('#t6').css('text-decoration', 'none');
   $('#t5').css('text-decoration', 'underline');
   $('#toggleText').html("<h5>David Bernstein: E'17</h5><h5>Joseph Cirone: E'17</h5><h5>Matthew Felsenfeld: A'16</h5><h5>Morgan Gellert: E'17</h5><h5>Ryan Hastings-Echo: A'17</h5><h5>Ryan Havens: A'17</h5><h5>Justin Lee: A'17</h5><h5>Matthew Mazzarella: A'17</h5><h5>Jacob Mittleman: A'17</h5><h5> Nikhil Nandagopal: A'17</h5><h5>Andrew Narahara: E'17</h5><h5>Benjamin Pall: E'17</h5><h5>Jonathan Sirota: A'17</h5>");
+  $('#toggleText h5').css('font-size', h5font+'px');
 }
 
 function togText6() {
@@ -385,4 +392,5 @@ function togText6() {
   $('#t5').css('text-decoration', 'none');
   $('#t6').css('text-decoration', 'underline');
   $('#toggleText').html("<h5>Arlo Clarke: E'17</h5><h5>Joshua Insler: A'17</h5><h5>James Lacitignola: A'17</h5><h5>Kevin Meli: E'17</h5><h5>Benjamin Shipley: A'17</h5><h5>Trevor Vassallo: E'16</br></br></br></br></br></br></h5>");
+  $('#toggleText h5').css('font-size', h5font+'px');
 }
